93a47098d2ff5651fc912706bf208c834fb3c035
YuriCat <a.a.b.a.b.c.a.b.c.d.abcd1234@gmail.com>
2019-07-09 18:30:18 +0900

    apply mask to single-head attention net by TF

--- tensorflow/attention.py
+++ tensorflow/attention.py
@@ -1,8 +1,8 @@
 import numpy as np
 import tensorflow as tf
 
-def single_head_self_attention(query, keys, out_features, self_specialization=True,
-                               projection=True, layer_norm=True, residual=True):
+def single_head_self_attention(query, keys, out_features, mask=None, self_specialization=True,
+                               projection=False, layer_norm=True, residual=True):
     """
     Args:
         query (torch.Tensor): [batch, element_size, in_features]
@@ -17,8 +17,6 @@ def single_head_self_attention(query, keys, out_features, self_specialization=Tr
     # [batch, element_size, out_features] に変換
     Q = tf.layers.dense(query, out_features, use_bias=False)
     K = tf.layers.dense(keys, out_features, use_bias=False)
-    #Q = tf.layers.dense(query, in_features, use_bias=False)
-    #K = keys
     V = tf.layers.dense(keys, out_features, use_bias=False)
     K = tf.transpose(K, [0, 2, 1])
 
@@ -28,7 +26,12 @@ def single_head_self_attention(query, keys, out_features, self_specialization=Tr
         attention *= 1 - tf.eye(int(element_size))
         attention += tf.matrix_diag(tf.squeeze(tf.layers.dense(query, 1), -1))
     attention *= float(int(in_features)) ** -0.5
+    if mask is not None:
+        # 大きな負の値にしてsoftmaxで0が付くように
+        attention += tf.expand_dims(1 - mask, -2) * -2e32
     attention = tf.nn.softmax(attention, axis=-1)
+    if mask is not None:
+        attention *= tf.expand_dims(mask, -1)
 
     # output は [batch, element_size, out_features] に変換
     output = tf.matmul(attention, V)
@@ -83,27 +86,36 @@ def self_attention(query, keys, out_features, out_heads,
 
     return output
 
-def graph(x):
+def graph(x, mask):
     h = x
     filters = 128
-    h = tf.nn.relu(self_attention(h, h, filters, 16, layer_norm=False, residual=False))
+    #h = tf.nn.relu(self_attention(h, h, filters, 16, mask, layer_norm=False, residual=False))
     #for _ in range(2):
     #  h = tf.nn.relu(self_attention(h, h, filters, 16, layer_norm=False))
     #h = self_attention(h, h, 2, 2, layer_norm=False, residual=False)
-    h = single_head_self_attention(h, h, 2, layer_norm=False, residual=False)
+    h = single_head_self_attention(h, h, 2, mask, layer_norm=False, residual=False)
     return h
 
-def loss(y, y_):
-    return tf.reduce_mean(tf.reduce_sum(tf.square(y - y_), axis=-1) ** 0.5)
+def dot(a, b):
+    return tf.squeeze(tf.matmul(tf.expand_dims(a, -2), tf.expand_dims(b, -1)), -1)
+
+def mask_max(a, b):
+    pass #return a - 
+
+def loss(y, y_, mask):
+    norm = tf.pow(tf.reduce_sum(tf.square(y - y_), axis=-1) + 1e-16, 0.5)
+    #norm = tf.reduce_sum(tf.square(y - y_), axis=-1)
+    return tf.reduce_mean(norm * mask)
 
 sess = tf.Session()
 
-elements = 5
+elements = 3
 x_ = tf.placeholder(tf.float32, [None, elements, 2])
 y_ = tf.placeholder(tf.float32, [None, elements, 2])
+mask_ = tf.placeholder(tf.float32, [None, elements])
 
-g = graph(x_)
-l = loss(g, y_)
+g = graph(x_, mask_)
+l = loss(g, y_, mask_)
 #optimization = tf.train.GradientDescentOptimizer(learning_rate=1e-3).minimize(l)
 optimization = tf.train.AdamOptimizer(learning_rate=1e-3).minimize(l)
 
@@ -112,33 +124,54 @@ print([int(np.prod(v.shape)) for v in tf.trainable_variables()])
 sess.run(tf.global_variables_initializer())
 
 x = np.random.random((1, elements, 2))
-print(sess.run(g, feed_dict={x_:x}))
-print(sess.run(g, feed_dict={x_:x[:, ::-1, :]}))
+msk = np.random.randint(0, 2, (1, elements))
+print(x)
+print(msk)
+print(sess.run(g, feed_dict={x_:x, mask_:msk}))
+print(sess.run(g, feed_dict={x_:x[:, ::-1, :], mask_:msk[:,::-1]}))
 
-points  = np.random.randn(100, elements, 2)
-points_v  = np.random.randn(100, elements, 2)
+points   = np.clip(np.random.randn(100, elements, 2), -10, 10)
+points_v = np.clip(np.random.randn(100, elements, 2), -10, 10)
+mask     = np.random.randint(0, 2, (100, elements))
+mask_v   = np.random.randint(0, 2, (100, elements))
+
+def mask_mean(a, mask):
+    sum_mask = np.expand_dims(mask.sum(axis=-1), -1)
+    mean_mask = np.true_divide(mask, sum_mask)
+    mean_mask[~np.isfinite(mean_mask)] = 0
+    return np.sum(a * np.expand_dims(mean_mask, -1), axis=-2)
 
 # 恒等写像
-def same(points):
+def identity(points, mask):
     return points
 
 # 重心
-def center(points):
-    return np.tile(np.mean(points, axis=1).reshape((-1, 1, 2)), [1, elements, 1])
+def center(points, mask):
+    return np.tile(mask_mean(points, mask).reshape((-1, 1, 2)), [1, elements, 1])
 
 # 最近傍点
-def nearest(points):
+def nearest(points, mask):
     distance = np.stack([np.linalg.norm(points[:,i:(i+1),:] - points, axis=-1) for i in range(elements)], axis=1)
     nearest_idx = np.argmin(distance + np.diag(np.ones(elements) * float('inf')), axis=-1)
     # https://stackoverflow.com/questions/23435782/numpy-selecting-specific-column-index-per-row-by-using-a-list-of-indexes
     return np.stack([points[np.arange(len(points)), nearest_idx[:,i]] for i in range(elements)], axis=1)
 
 func = nearest
-x, y = points, func(points)
-x_v, y_v = points_v, func(points_v)
+x, y = points, func(points, mask)
+x_v, y_v = points_v, func(points_v, mask_v)
 
 for _ in range(10000):
-    print(sess.run(l, feed_dict={x_:x, y_:y}), sess.run(l, feed_dict={x_:x_v, y_:y_v}))
-    print(sess.run(g, feed_dict={x_:x[0:1]}))
-    print(y[0:1])
-    sess.run(optimization, feed_dict={x_:x, y_:y})
+    loss_train = sess.run(l, feed_dict={x_:x, y_:y, mask_:mask})
+    loss_val = sess.run(l, feed_dict={x_:x_v, y_:y_v, mask_:mask_v})
+    print(loss_train, loss_val)
+    if np.isnan(loss_train):
+        for i in range(100):
+            print(x[i])
+            print(mask[i])
+            print(y[i])
+        input()
+    print(x[:1])
+    print(mask[:1])
+    print(sess.run(g, feed_dict={x_:x[:1], mask_:mask[:1]}))
+    print(y[:1])
+    sess.run(optimization, feed_dict={x_:x, y_:y, mask_:mask})
